f329b8bece7e40e2d6d0f20b261a7ceb784dec9a
MarcOlivier91 <smarc.mailpro@gmail.com>
2025-06-19 14:08:49 +0200

    styling delivery card

--- src/components/DeliveryCard.tsx
+++ src/components/DeliveryCard.tsx
@@ -13,12 +13,17 @@ export default function DeliveryCard(props: DeliveryCardProps) {
   return (
     <View>
       <TouchableOpacity style={styles.card}>
-        <View>
-          <MaterialCommunityIcons name="truck" size={24} />
-          <Text>Livraison #{props.deliveryId}</Text>
-          <Text>{props.entreprise}</Text>
-          <Text>{props.status}</Text>
-          <Text>{props.date}</Text>
+        <View style={styles.cardContainer}>
+          <MaterialCommunityIcons name="truck" size={30} />
+          <View>
+            <Text style={styles.livraisonIdText}>
+              Livraison #{props.deliveryId}
+            </Text>
+            <Text>
+              {props.entreprise} | {props.date}
+            </Text>
+            <Text>{props.status}</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </View>
@@ -28,14 +33,27 @@ export default function DeliveryCard(props: DeliveryCardProps) {
 const styles = StyleSheet.create({
   card: {
     backgroundColor: "#fff",
-    padding: 40,
+    padding: 10,
     borderRadius: 10,
-    marginVertical: 10,
+    marginVertical: 15,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 2,
     marginHorizontal: 40,
+  },
+
+  cardContainer: {
+    padding: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+
+  livraisonIdText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
   },
 });
