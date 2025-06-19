aee558ae280a40243372b1e9af06b06ec054409e
MarcOlivier91 <smarc.mailpro@gmail.com>
2025-06-19 14:09:13 +0200

    WIP created new page for deliveries details

---
+++ src/app/deliveries/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function deliveryDetails() {
+  return (
+    <View>
+      <Text>Détails de la livraison</Text>
+    </View>
+  );
+}
