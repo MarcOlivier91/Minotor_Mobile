f5e9698e18346e2ff3336bf30f2534f745aaf90c
MarcOlivier91 <smarc.mailpro@gmail.com>
2025-06-19 17:01:22 +0200

    renamed function

--- src/app/deliveries/[id].tsx
+++ src/app/deliveries/[id].tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function deliveryDetails() {
+export default function DeliveryDetails() {
   return (
     <View>
       <Text>Détails de la livraison</Text>
